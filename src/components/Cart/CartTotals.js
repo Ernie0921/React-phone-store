58291ebbe8ef7d917d259c8db372f02d3490ae50
Ernesto Carrillo <ernesto.carrillo10@icloud.com>
2021-05-04 00:56:30 -0400

    adding clear button to cart page

---
+++ src/components/Cart/CartTotals.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function CartTotals(value) {
+    const{cartSubtotal, cartTax, cartTotal, clearCart} = value;
+
+    return (
+        <React.Fragment>
+         <div className="container">
+            <div className="row">
+                <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+
+                
+                
+                </div>
+            
+            </div>
+         
+         
+         </div>
+        
+        </React.Fragment>
+    )
+}
